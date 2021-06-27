bb2fc13f27e37720790af62f906dfe8e9df3bc6a
Pranav <pranavmendi@gmail.com>
2021-06-27 07:00:14 +0530

    Add basic validation of checking empty name field and showing error on submission

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SimpleInput = (props) => {
+	const [enteredName, setEnteredName] = useState('');
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+	const nameInputChangeHandler = (e) => {
+		setEnteredName(e.target.value);
+	};
+
+	const formSubmissionHandler = (e) => {
+		e.preventDefault();
+		setEnteredNameTouched(true);
+		if (enteredName.trim() === '') {
+			setEnteredNameIsValid(false);
+			return;
+		}
+		setEnteredNameIsValid(true);
+
+		// console.log(enteredName);
+		setEnteredName('');
+	};
+
+	const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
+	const nameInputClasses = nameInputIsInvalid
+		? 'form-control invalid'
+		: 'form-control';
+
 	return (
-		<form>
-			<div className='form-control'>
+		<form onSubmit={formSubmissionHandler}>
+			<div className={nameInputClasses}>
 				<label htmlFor='name'>Your Name</label>
-				<input type='text' id='name' />
+				<input
+					type='text'
+					id='name'
+					onChange={nameInputChangeHandler}
+					// onBlur={nameInputBlurHandler}
+					value={enteredName}
+				/>
+				{nameInputIsInvalid && (
+					<p className='error-text'>Name must not be empty.</p>
+				)}
 			</div>
 			<div className='form-actions'>
 				<button>Submit</button>
